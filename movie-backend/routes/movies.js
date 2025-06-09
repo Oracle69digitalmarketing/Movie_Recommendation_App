7c1b2a2579db94114383013503006881784be714
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-06-09 12:10:29 +0100

    Update movies.js

--- movie-backend/routes/movies.js
+++ movie-backend/routes/movies.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const auth = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
-// Save favorite movie
-router.post('/favorites', auth, async (req, res) => {
-  const user = await User.findById(req.user.id);
-  user.favorites.push(req.body.movie);
-  await user.save();
-  res.json({ favorites: user.favorites });
-});
-
-// Get favorites
-router.get('/favorites', auth, async (req, res) => {
-  const user = await User.findById(req.user.id);
-  res.json({ favorites: user.favorites });
+// Example protected route
+router.get('/', authMiddleware, (req, res) => {
+  res.json({ message: 'Movies route secured' });
 });
 
 module.exports = router;
