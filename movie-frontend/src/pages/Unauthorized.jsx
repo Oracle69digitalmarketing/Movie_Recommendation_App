a00bd2d5800555c177d11b8e405e2ad473dd7261
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-06-17 17:21:01 +0100

    Create Unauthorized.jsx

---
+++ movie-frontend/src/pages/Unauthorized.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Unauthorized = () => (
+  <div className="unauthorized">
+    <h2>403 - Unauthorized</h2>
+    <p>You do not have permission to access this page.</p>
+  </div>
+);
+
+export default Unauthorized;
