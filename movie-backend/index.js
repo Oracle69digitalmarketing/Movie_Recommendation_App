3ae7c2820d70d4a717679c520c55cd513c42d89d
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-06-18 01:15:56 +0100

    Update index.js

--- movie-backend/index.js
+++ movie-backend/index.js
@@ -30,3 +30,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+// At the bottom of index.js
+export default app;
