407360ac072df476878ed216654776d2d877880c
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-06-18 08:53:12 +0100

    Update watchlist.js

--- movie-backend/src/routes/watchlist.js
+++ movie-backend/src/routes/watchlist.js
@@ -1,17 +1,16 @@
 import express from 'express';
 import {
   addToWatchlist,
-  getWatchlist,
-  removeFromWatchlist
+  getUserWatchlist,
+  removeFromWatchlist,
 } from '../controllers/watchlistController.js';
+
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.use(authMiddleware);
-
-router.post('/', addToWatchlist);                // Add to watchlist
-router.get('/', getWatchlist);                   // Get user's watchlist
-router.delete('/:movieId', removeFromWatchlist); // Remove from watchlist
+router.post('/', authMiddleware, addToWatchlist);
+router.get('/', authMiddleware, getUserWatchlist);
+router.delete('/:movieId', authMiddleware, removeFromWatchlist);
 
 export default router;
