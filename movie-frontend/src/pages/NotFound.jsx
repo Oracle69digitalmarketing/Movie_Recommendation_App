928315ad692a788e82153bda12dd7ad36b15ab52
Prince Adewale Adewumi <82568973+Oracle69digitalmarketing@users.noreply.github.com>
2025-06-21 09:42:14 +0100

    Create NotFound.jsx

---
+++ movie-frontend/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
